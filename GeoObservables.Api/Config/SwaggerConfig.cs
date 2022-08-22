01791b6b61fab9b02f81a3e6a2716fba479c48dd
A11Carballo <albertocarballo.g@gmail.com>
2022-08-22 22:37:53 +0200

    swagger config

---
+++ GeoObservables.Api/Config/SwaggerConfig.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GeoObservables.Api.Config
+{
+    public static class SwaggerConfig
+    {
+
+        public static IServiceCollection AddRegistration(this IServiceCollection services)
+        {
+            var basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            var xmlPath = Path.Combine(basePath, "GeoObservables.Api.xml");
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "GeoObservables API V1",
+                    Version = "v1",
+                    Description = "GeoObservable API net core",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Alberto Carballo",
+                        Email = "alberto.carballo@hotmail.com",
+                        Url = new Uri("https://twitter.com/acarballo")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under XXX",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                c.IncludeXmlComments(xmlPath);
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
+
+            return services;
+
+        }
+    }
+}
--- GeoObservables.Api/Program.cs
+++ GeoObservables.Api/Program.cs
@@ -1,3 +1,4 @@
+using GeoObservables.Api.Config;
 using GeoObservables.Api.CrosssCutting.Register;
 using GeoObservables.Api.DataAccess;
 using GeoObservables.Api.DataAccess.Contracts;
@@ -5,9 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var basePath = System.AppDomain.CurrentDomain.BaseDirectory;
-var xmlPath = Path.Combine(basePath, "GeoObservables.Api.xml");
 
 ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
 IWebHostEnvironment environment = builder.Environment;
@@ -18,62 +16,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { 
-        Title = "GeoObservables API V1", 
-        Version = "v1",
-        Description = "GeoObservable API net core",
-        Contact = new OpenApiContact
-        {
-            Name = "Alberto Carballo",
-            Email = "alberto.carballo@hotmail.com",
-            Url = new Uri("https://twitter.com/acarballo")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Use under XXX",
-            Url = new Uri("https://example.com/license")
-        }
-    });
-    c.IncludeXmlComments(xmlPath);
-    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                      new OpenApiSecurityScheme
-                      {
-                        Reference = new OpenApiReference
-                          {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                          },
-                          Scheme = "oauth2",
-                          Name = "Bearer",
-                          In = ParameterLocation.Header,
-
-                        },
-                        new List<string>()
-                    }
-                });
-
-});
-
 builder.Services.AddScoped<IGeoObservablesDBContext, GeoObservablesDBContext>();
 
 builder.Services.AddDbContext<GeoObservablesDBContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 IoCRegister.AddRegistration(builder.Services);
+
+SwaggerConfig.AddRegistration(builder.Services);
 
 var app = builder.Build();
 
@@ -81,10 +30,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(cfg =>
+    app.UseSwaggerUI(c =>
     {
-        //cfg.SwaggerEndpoint("/swagger/GeoObservables/swagger.json", "GeoObservables Analyze");
-        //cfg.RoutePrefix = String.Empty;
+        c.OAuthClientId("swagger-ui");
+        c.OAuthClientSecret("swagger-ui-secret");
+        c.OAuthRealm("swagger-ui-realm");
+        c.OAuthAppName("Swagger UI");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeoObservable API V1");
+
     });
 }
 
