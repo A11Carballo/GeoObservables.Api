0bea1909c7755de6c19eb9bc2b9156e8290bd537
A11Carballo <albertocarballo.g@gmail.com>
2022-08-23 20:10:36 +0200

    [Required]

--- GeoObservables.Api/ViewModels/UsersViewModel.cs
+++ GeoObservables.Api/ViewModels/UsersViewModel.cs
@@ -1,13 +1,17 @@
-﻿namespace GeoObservables.Api.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeoObservables.Api.ViewModels
 {
     public class UsersViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string User { get; set; }
+        [Required]
         public string Mail { get; set; }
+        [Required]
         public string Password { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
+        [Required]
         public int IdRole { get; set; }
         public string Description { get; set; }
         public bool Active { get; set; }
