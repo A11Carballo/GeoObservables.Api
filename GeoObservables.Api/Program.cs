b60cb630576cb64f1d397509a84fcea4ff19d126
A11Carballo <albertocarballo.g@gmail.com>
2022-08-24 14:51:10 +0200

    wip

--- GeoObservables.Api/Program.cs
+++ GeoObservables.Api/Program.cs
@@ -10,7 +10,6 @@ ConfigurationManager configuration = builder.Configuration; // allows both to ac
 IWebHostEnvironment environment = builder.Environment;
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
