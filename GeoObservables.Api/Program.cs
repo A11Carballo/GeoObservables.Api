e7f5dfa62dde51e14e92c96bdca538b8e61f5678
A11Carballo <albertocarballo.g@gmail.com>
2022-08-28 20:31:03 +0200

    Use session to allow manage session data

--- GeoObservables.Api/Program.cs
+++ GeoObservables.Api/Program.cs
@@ -12,7 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
 IWebHostEnvironment environment = builder.Environment;
 
-/*builder.Services.AddSession(options =>
+builder.Services.AddMvc()
+       .AddSessionStateTempDataProvider();
+
+builder.Services.AddCors();
+
+builder.Services.AddSession(options =>
 {
     // default session time out is 20 minutes 
     // but we can set it to any time span 
@@ -21,7 +26,7 @@ IWebHostEnvironment environment = builder.Environment;
     // allows to use the session cookie 
     // even if the user hasn't consented 
     options.Cookie.IsEssential = true;
-});*/
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -54,6 +59,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 });
 
 var app = builder.Build();
+
+// allow to manage user session data 
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
