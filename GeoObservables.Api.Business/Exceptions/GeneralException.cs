c2b9f1ffdc7705eb0eef2353e6aebfb78d7cd703
A11Carballo <albertocarballo.g@gmail.com>
2022-12-10 20:29:00 +0100

    Results and Exceptions

---
+++ GeoObservables.Api.Business/Exceptions/GeneralException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoObservables.Api.Business.Exceptions
+{
+    public class GeneralException : Exception 
+    {
+        public GeneralException() : base("An error has ocurred")
+        {
+
+        }
+
+        public GeneralException(Exception ex) : base("An error has ocurred", ex)
+        {
+
+        }
+
+        public GeneralException(string message) : base(message)
+        {
+
+        }
+
+        public GeneralException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
